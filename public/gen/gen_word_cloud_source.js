c8a2dacf1a3eda14d956e77a960e4f4b349fea29
bingxing <anastagizeno@gmail.com>
2025-04-22 14:41:23 +0800

    tick-tock

---
+++ public/gen/gen_word_cloud_source.js
@@ -0,0 +1,73 @@
+var words = ["左派+权力过程+泰德·卡辛斯基+社会+技术+体系+人类+权力+问题+人们+无法+个人+过程+目标+心理+工业+工作+社会化+活动+美国+运动+过度+影响+革命+思想+生活+替代+经济+黑人+科学家+政府+政治+现代人+人口+公司+权利+环境+变化+儿童+机器+原则+发展+群体+宣传+宗教+工业体系+方法+假设+产生+大众+革命者+理性+世界+中国+孩子+安全+冲突+生物学+科学+身体+动机+机会+文化+主管+基因工程+态度+先生+痛苦+白人+系统+工具+原始人+药物+地位+历史+青少年+国家+管制+价值观+传统+道德+运转+小区+设备+公众+诸如+压力+基因+约束+利用+精英+精力+持续+官员+家庭+能力+动物+媒体+民族+时间+教育+俄国+形式+犯罪+食物+中断+地方+英国+任务+企业+信徒+方面+力量+广告+信仰+行动+民主+服务+手段+普通人+欧洲+工业化+心理学+规定+化学家+大力+预测+计划+基本+原因+类人+银行职员+造物+残疾人+方向+领袖+趋势+设计+事实+培训+地区+营销+信息+特征+机动+经验+责任+利益+致力于+法律+无力感+基础+高尔夫+广告业+主张+疾病+宪法+威胁+罗马+博士+运作+证明+城市+印第安+安东尼+东西+范围+领域+决策+实际+少数民族+分子+控制权+子女+经历+中产阶级+工厂+贵族+法国+研究+文明+花费+妇女+成就+数学家+弱化+计算器+泰勒+男性+化学品+观念+种族+准则+左倾+合作+人道主义+爱好者+敌意+设想+共产党+平权+邪教+体验+有序+时期+当局+负面+加盟+合理+殖民地+生物+集体+思维+声称+训练+症状+机构+苏联+关系+工程师+电极+理想+自主性+植入+尝试+福利+热心+所需+集体主义+衣服+金钱+产品+协议+弱者+非裔+自卑感+保险+精神+激进分子+后果+监控+物质+暴力+价值+娱乐+制度+概念+法学院+疗法+事物+农民+原始社会+整体+抵抗+生理+杀虫剂+昆虫+发电机+技术人员+分类+公民+漫长+市场营销+核电站+科学研究+基本原理+轨道+审查+离群索居+生命周期+琐屑+资格+效率+法律制裁+黑奴+肌肉+罗伯特+总统+机动车辆+患病率+集权主义+猎物+资本主义+方圆+海洋生物+战士+拉丁美洲+欺诈+种植园+小球+泰德+国会+余地+临床+意图+武器+帝国主义+小家庭+地球+意志+借口+风险+顾客+日本+表亲+生产+观点+西尔万学习中心+积极分子+专家+纽约日报+监管+民意+正义+主义+刘易斯+追随者+传播+定义+困难+育儿+医学+高度+习惯+大脑+竞争+艺术+家长+仇恨+饥渴+社会主义+理由+状态+行使+出版社+运用+意识+成员+猎人+受害者+土著+工人+遭遇+销售员+道德规范+失业+学术+致癌物+危险+机制+事业+命令+根源+政体+机动车+想象+被动+美洲+男孩+命运+知识分子+联系+强者+区别+逻辑学家+占上风+哲学+人性+局部+直觉+压制+裕仁天皇+预期+智商+无政府主义+抗抑郁+作家+人脑+高管+掌权+记忆+感兴趣+纳粹+温室效应+声望+指数+代议+癌症+越南+照料+政客+规模+支配+自主权+目的+表面+内心+学校+澳洲+民族主义+中上阶层+精神疾病+阶梯+警告+理论家+新手+环境污染+失败主义+投身于+集邮+保守派+销售+女性+设施+洗脑+购物+丁烷+局限于+所有人+水蛭+社团+热情+信念+方案+计算机+反应+冰箱+独裁者+专业+水平+可能性+读者+意义+现实+机械+全国+音乐+议会+后者+调和+物理+上将+权益+规范+词汇+结构+墨西哥+食品+智力+空间+交通规则+指向+数据+模式+无情+社群+任命+亚洲+现存+资产阶级+卓越+蓝领+斗争+类型+住处+海量+标准+代表+同情+城市化+温驯+部落+更糟+工匠+上帝+大大增加+技能+文章+红绿灯+阶段+测试+巴兰+枪支+感觉+单凭+光阴+解体+黑社会+马萨诸塞州+缺点+易感性+意见+极少数人+军事+形态+危害+本质+时代+患者+远程+水源+秩序+理念+申请者+意大利+环境退化+远东+根本就是+傻子+君主制+北朝鲜+计算机网络+新闻出版+危机+行事+透顶+理论+割草机+私人+法则+卫生系统+边境地区+保护者+罪犯+原教旨主义+大费周章+品行不端+政治腐败+全面+社会党+牌照+实则+朋友+效仿+生养+黑帮+教友+生理需求+客观现实+目的性+领导+抑郁症+真理+出生率+企业主+公务员+垃圾+北美+考试+书籍+体面+错误+教育家+声势+电激使+措施+灾难性+部位+电激+海曼+基本上+饥荒+教育工作者+满足感+牧人+宗教信仰+渔民+乡村+内向+底层+秘密警察+体质+高明+呆子+二者+施加压力+人民+证据+昆虫学家+导向+大学教授+小溪+心理学家+按计划+科学知识+筹划+莫斯科+个体+人才+犯罪团伙+组织化+杂志+谈论+邪教徒+大体+铺设+竞争力+女权主义者+数量+心理压力+极权主义+归类+效应+运输+印度+现代农业+危险性+智能+臭氧层+灌输+空洞+学费+实质+土地+现代化+铜线+效力+乌托邦+关心+化学+胡汉民+人口比例+国民党+新品种+测验+天文学家+产物+节奏+企业家+代价+性格+狩猎+元素+强制执行+管理+军事行动"];
+function wordCloud(selector) {
+    var fill = d3.scale.category20();
+    var svg = d3.select(selector).append("svg").attr("width", 1000).attr("height", 600).attr("viewBox", "0 0 1000 600").attr("preserveAspectRatio", "xMidYMid meet").append("g").attr("transform", "translate(500,300)");
+    function draw(words) {
+        var cloud = svg.selectAll("g text").data(words,
+        function(d) {
+            return d.text
+        });
+        cloud.enter().append("text").style("font-family", "Microsoft Yahei, sans-serif").style("font-weight", "bold").style("fill",
+        function(d, i) {
+            return fill(i)
+        }).attr("text-anchor", "middle").attr('font-size', 1).text(function(d) {
+            return d.text
+        });
+        cloud.transition().duration(600).style("font-size",
+        function(d) {
+            return d.size + "px"
+        }).attr("transform",
+        function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"
+        }).style("fill-opacity", 1);
+        cloud.exit().transition().duration(300).style('fill-opacity', 1e-6).attr('font-size', 1).remove()
+    }
+    return {
+        update: function(words) {
+            d3.layout.cloud().size([1000, 600]).words(words).padding(1).rotate(function() {
+                return 0
+            }).font("Microsoft Yahei, sans-serif").fontSize(function(d) {
+                return d.size
+            }).on("end", draw).start()
+        }
+    }
+}
+function getWords(i) {
+    var cals = words[i].replace(/[!\,:;\?]/g, '').split('+');
+    var randomWords = [];
+    while (randomWords.length < 60) {
+        var randomIndex = Math.floor(Math.random() * cals.length);
+        if (!randomWords.includes(cals[randomIndex])) {
+            randomWords.push(cals[randomIndex])
+        }
+    }
+    return randomWords.map(function(d) {
+        return {
+            text: d,
+            size: generateRandomInteger()
+        }
+    })
+}
+function showNewWords(vis, i) {
+    i = i || 0;
+    vis.update(getWords(i++%words.length));
+    setTimeout(function() {
+        showNewWords(vis, i)
+    },
+    2400)
+}
+function customRandom() {
+    const rand = Math.random();
+    if (rand < 0.5) {
+        return 20 + Math.random() * 40
+    } else if (rand < 0.75) {
+        return 20 - Math.sqrt(Math.random()) * 10
+    } else {
+        return 100 - Math.sqrt(Math.random()) * 40
+    }
+}
+function generateRandomInteger() {
+    return Math.round(customRandom())
+}
+var myWordCloud = wordCloud('.about-page');
+showNewWords(myWordCloud);
